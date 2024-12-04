3a5f0e410ec4440aaf4307fe9cf236a2957466ab
loki160 <lokik2843@gmail.com>
2024-12-04 17:27:52 +0530

    Update sdemo.sql

--- sdemo.sql
+++ sdemo.sql
@@ -1,10 +1,4 @@
 -- Create snowpark procedure
-create or replace procedure hello()
-    returns string
-    language python 
-    runtime_version= '3.8'
-    packages=('snowflake-snowpark-python')
-    imports=('@tutorial/branches/main/examples/hello.py')
-    handler='hello.main';
-
-call hello();
+begin 
+return 'hello snow';
+end;
